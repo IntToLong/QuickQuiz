046ac5bfc58a9f242b7506689a6308ab4956b741
Nataliia Liubynets <natalialiubynets@gmail.com>
2025-05-15 13:44:04 +0200

    fix(progress-bar): enable animation on load

--- components/result/progress-bar/progress-bar.js
+++ components/result/progress-bar/progress-bar.js
@@ -1,19 +1,19 @@
-
+'use client';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+
+import { useEffect, useState } from 'react';
 
 import { easeQuadInOut } from 'd3-ease';
 import styles from './progress-bar.module.css';
 import ProgressProvider from './progress-provider';
 
-
 export default function ProgressBar({ percentOfCorrectAnswers }) {
-    	return (
+	return (
 		<div className={styles.result_progressCircle}>
 			<ProgressProvider
 				valueStart={0}
 				valueEnd={percentOfCorrectAnswers}
-				duration={10}
 				easingFunction={easeQuadInOut}>
 				{(value) => {
 					const roundedValue = Math.round(value);
@@ -22,10 +22,10 @@ export default function ProgressBar({ percentOfCorrectAnswers }) {
 							value={value}
 							text={`${roundedValue}%`}
 							styles={buildStyles({
-								pathTransition: 'none',
 								textColor: '#bababa',
 								pathColor: '#048c80',
 								trailColor: '#bababa',
+								pathTransitionDuration: 1,
 							})}
 						/>
 					);
